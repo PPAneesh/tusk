2fbfbf3d987ad0fd3c60bcca2af397cca5e8949a
PPAneesh <45414599+PPAneesh@users.noreply.github.com>
2019-01-25 17:39:30 +0530

    latest changes

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 class test {
 	public static void main(String[] args)
 	{
-		System.out.println("My test project");
+		System.out.println("My test project by including jenkins CI for testing ");
 	}
 }
 
